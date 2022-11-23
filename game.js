6a30afac7c0e583ca3808ab46acf461ed3c3a4ab
Naman Jha <jhanaman786@gmail.com>
2022-11-23 17:11:54 +0530

    added animation and sound

--- game.js
+++ game.js
@@ -6,9 +6,15 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).click(function () {
+        $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    });
+
+    $("#" + randomChosenColor).click(function () {
+        var audio = new Audio("sounds/" + randomChosenColor + ".mp3")
+        audio.play();
+    });
 }
-
-
 
 
 
