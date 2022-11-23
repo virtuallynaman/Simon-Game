f134673a6ad44d49f324d60db148a3ffcb3cf26b
Naman Jha <jhanaman786@gmail.com>
2022-11-23 17:42:00 +0530

    Storing user button clicked pattern

--- game.js
+++ game.js
@@ -1,20 +1,29 @@
-var gamePattern = []
+var gamePattern = [];
+var userClickedPattern = [];
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 
+//Generating random number
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random()*4);
+    var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    //Adding animation
     $("#" + randomChosenColor).click(function () {
         $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
     });
-
+    //Adding sound
     $("#" + randomChosenColor).click(function () {
         var audio = new Audio("sounds/" + randomChosenColor + ".mp3")
         audio.play();
     });
 }
+
+//Storing user button clicked pattern
+$(".btn").click(function event () {
+    var userChosenColor = (this.id);
+    userClickedPattern.push(userChosenColor);
+})
 
 
 
